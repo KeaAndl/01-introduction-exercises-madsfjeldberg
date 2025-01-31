a353ca15934146e9b8039388ee62785c9bc34770
madsfjeldberg <mafj0001@stud.kea.dk>
2025-01-31 12:28:12 +0100

    finished work

--- 2._Strings.js
+++ 2._Strings.js
@@ -7,15 +7,17 @@ const numberTwo = "2.30";
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
 
-console.log(parseFloat(numberOne) + parseFloat(numberTwo))
+console.log(parseFloat(numberOne) + parseFloat(numberTwo));
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
 
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
 
-result = (parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)).toFixed(2)
-console.log(result)
+result = (parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)).toFixed(
+  2
+);
+console.log(result);
 // --------------------------------------
 // Exercise 5 - Decimals and average
 
@@ -25,21 +27,17 @@ const three = 98;
 
 // Show in the console the avg. with 5 decimals
 
+let total = one + two + three;
+let average = total / 3;
 
-
-let total = one + two + three
-let average = total / 3
-
-console.log(average.toFixed(5))
+console.log(average.toFixed(5));
 // --------------------------------------
 // Exercise 6 - Get the character by index
 
 const letters = "abc";
 // Get me the character "c"
 
-
-
-console.log(letters.charAt(2))
+console.log(letters.charAt(2));
 
 // --------------------------------------
 // Exercise 7 - Replace
@@ -48,8 +46,5 @@ const fact = "You are learning javascript!";
 
 // capitalize the J in Javascript
 
-console.log(fact.replace("j", "J"))
+console.log(fact.replace("j", "J"));
 // --------------------------------------
-
-
-
